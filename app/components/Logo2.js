05aae0628f76148d035f82a55c1b0e434b0aaec9
Ognjen Stefanovic <ognjen.stefanovic@codaxy.com>
2021-07-29 14:17:11 +0200

    Color change

--- app/components/Logo2.js
+++ app/components/Logo2.js
@@ -4,10 +4,10 @@ import '../icons/index';
 export const Logo2 = ({ className }) => (
    <cx>
       <Link href="~/" class="flex items-center" className={className}>
-         <div class="p-2 rounded-full bg-blue-500 text-white">
+         <div class="p-2 rounded-full bg-red-600 text-white">
             <Icon name="view-grid-add" class="w-6 h-6 block" />
          </div>
-         <div class="ml-3 text-lg font-bold text-blue-500">Widgets Showcase</div>
+         <div class="ml-3 text-lg font-bold text-red-600">Widgets Showcase</div>
       </Link>
    </cx>
 );

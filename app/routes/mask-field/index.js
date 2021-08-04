e36c72993ae1e610e69a986f2914ffd8bbf38bc8
Ognjen Stefanovic <ognjen.stefanovic@codaxy.com>
2021-08-04 10:31:43 +0200

    Example. work in progress

--- app/routes/mask-field/index.js
+++ app/routes/mask-field/index.js
@@ -1,21 +1,21 @@
 import { Section, TextField } from 'cx/widgets';
-import MaskInputField from 'cx-mask-input-field';
+import { LabelsLeftLayout } from 'cx/src/ui';
+import MaskInputField from '../../components/cx-mask-input-field';
 
 export default (
    <cx>
       <div class="bg-gray-50 overflow-auto">
          <div class=" w-[1000px] mx-auto my-16 text-gray-700 bg-white px-20 py-16 border rounded relative">
-            <h1 class="text-5xl font-bold">CxJS + Tailwind CSS </h1>
-            <p class="mt-2 text-gray-400 text-lg">Use CxJS, Tailwind CSS, and Heroicons for success!</p>
+            <h1 class="text-5xl font-bold">Mask Input Field</h1>
+            <p class="mt-2 text-gray-400 text-lg">Package: react-input-mask</p>
 
             <p class="mt-12 text-lg text-gray-700">
-               CxJS and Tailwind CSS mix really well together. CxJS brings JavaScript based application elements such as
-               widgets, charts, state management, and routing, while Tailwind CSS offers a way to rapidly combine these
-               elements into higher-order visual structures - toolbars, sections, layouts, pages, etc.
+               When you need to have a mask on your input field, you can use this component. Unfortunately, the regex is
+               not yet supported.
             </p>
-            <Section>
-               <MaskInputField value-bind="data" mask="99.99.99.99" maskPlaceholder="_" />
-            </Section>
+            <div class="mt-5" layout={LabelsLeftLayout}>
+               <MaskInputField label="IP Address" value-bind="data" mask="999.999.999.999" placeholder="192.168.1.1" />
+            </div>
          </div>
       </div>
    </cx>
